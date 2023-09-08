70fc41c35234a8e419e2c7cc6406a9dc0e5d8870
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-09-08 17:00:58 +0300

    Первый урок

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,45 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[10];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        Resume[] tempStorage = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            tempStorage[i] = storage[i];
+        }
+        return tempStorage;
     }
 
-    int size() {
+    public int size() {
         return 0;
     }
 }
