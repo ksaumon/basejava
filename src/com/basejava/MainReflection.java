3b0a7168eb6bef549defa0d726f58792faac7665
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-09-18 09:29:06 +0300

    4 урок

--- src/com/basejava/MainReflection.java
+++ src/com/basejava/MainReflection.java
@@ -3,10 +3,9 @@ package com.basejava;
 import com.basejava.model.Resume;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws IllegalAccessException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
