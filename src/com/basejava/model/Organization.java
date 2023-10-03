2d9e80a19c25f01f25901057e123278b0d3811a7
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-03 18:58:10 +0300

    7 урок

--- src/com/basejava/model/Organization.java
+++ src/com/basejava/model/Organization.java
@@ -1,4 +1,38 @@
 package com.basejava.model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Organization {
+    private final String name;
+    private final String website;
+    private final List<Period> periods;
+
+    public Organization(String name, String website, List <Period> periods) {
+        this.name = name;
+        this.website = website;
+        this.periods = periods;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization that = (Organization) o;
+        return name.equals(that.name) && website.equals(that.website) && periods.equals(that.periods);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, website, periods);
+    }
+
+    @Override
+    public String toString() {
+        return "Organization{" +
+                "name='" + name + '\'' +
+                ", website='" + website + '\'' +
+                ", periods=" + periods +
+                '}';
+    }
 }
--- src/com/basejava/model/Period.java
+++ src/com/basejava/model/Period.java
@@ -1,4 +1,41 @@
 package com.basejava.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Period {
+    private final String specialityPosition;
+    private final String responsibilities;
+    private final LocalDate endDate;
+    private final LocalDate startDate;
+
+    public Period(String specialityPosition, String responsibilities, LocalDate endDate, LocalDate startDate) {
+        this.specialityPosition = specialityPosition;
+        this.responsibilities = responsibilities;
+        this.endDate = endDate;
+        this.startDate = startDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Period period = (Period) o;
+        return specialityPosition.equals(period.specialityPosition) && responsibilities.equals(period.responsibilities) && endDate.equals(period.endDate) && startDate.equals(period.startDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(specialityPosition, responsibilities, endDate, startDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Period{" +
+                "specialityPosition='" + specialityPosition + '\'' +
+                ", responsibilities='" + responsibilities + '\'' +
+                ", endDate=" + endDate +
+                ", startDate=" + startDate +
+                '}';
+    }
 }
