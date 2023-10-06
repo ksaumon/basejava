365ae5c67400bcf66689e4901594ed49e7d4e57c
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-06 17:00:20 +0300

    8 урок

--- src/com/basejava/storege/AbstractFileStorage.java
+++ src/com/basejava/storege/AbstractFileStorage.java
@@ -8,15 +8,15 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
-public class AbstractFileStorage extends AbstractStorage<File> {
+public class AbstractFileStorage extends AbstractStorage <File> {
     private File directory;
 
     protected AbstractFileStorage(File directory) {
         Objects.requireNonNull(directory, "directory must not be null");
-        if (!directory.isDirectory()) {
+        if (! directory.isDirectory()) {
             throw new IllegalArgumentException(directory.getAbsolutePath() + "is not directory");
         }
-        if (!directory.canRead() || !directory.canWrite()) {
+        if (! directory.canRead() || ! directory.canWrite()) {
             throw new IllegalArgumentException(directory.getAbsolutePath() + "is not readable/writable");
         }
         this.directory = directory;
@@ -34,7 +34,11 @@ public class AbstractFileStorage extends AbstractStorage<File> {
 
     @Override
     protected void doUpdate(Resume r, File file) {
-
+        try {
+            doWrite(r, file);
+        } catch(IOException e) {
+            throw new StorageException("File write error", r.getUuid(), e);
+        }
     }
 
     @Override
@@ -62,16 +66,27 @@ public class AbstractFileStorage extends AbstractStorage<File> {
 
     @Override
     protected void doDelete(File file) {
-
+        if (! file.delete()) {
+            throw new StorageException("File delete error", file.getName());
+        }
     }
 
     @Override
     public void clear() {
-
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                doDelete(file);
+            }
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        String[] list = directory.list();
+        if (list == null) {
+            throw new StorageException("Directory read error", null);
+        }
+        return list.length;
     }
 }
