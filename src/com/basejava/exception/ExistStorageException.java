b76d2a095dbb36490e397d03260d6eb540f6951b
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-15 15:29:56 +0300

    10 урок

--- src/com/basejava/exception/ExistStorageException.java
+++
@@ -1,7 +0,0 @@
-package com.basejava.exception;
-
-public class ExistStorageException extends StorageException {
-    public ExistStorageException(String uuid) {
-        super("Resume " + uuid + " already exist", uuid);
-    }
-}
--- src/com/basejava/model/Section.java
+++
@@ -1,6 +0,0 @@
-package com.basejava.model;
-
-import java.io.Serializable;
-
-public abstract class Section implements Serializable {
-}
