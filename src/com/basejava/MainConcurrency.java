7bcee10c56d611475e18028515bd48fb1bc1a4cb
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-17 18:56:06 +0300

    11 урок

--- src/com/basejava/MainConcurrency.java
+++ src/com/basejava/MainConcurrency.java
@@ -1,8 +1,5 @@
 package com.basejava;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainConcurrency {
     private int counter;
     private static final Object LOCK = new Object();
@@ -100,26 +97,29 @@ public class MainConcurrency {
 //    }
 
 //       join() вместо ожидания
-        final MainConcurrency mainConcurrency = new MainConcurrency();
-        List <Thread> threads = new ArrayList <>(THREADS_NUMBER);
-        for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
-                for (int j = 0; j < 100; j++) {
-                    mainConcurrency.inc();
-                }
-            });
-            thread.start();
-            threads.add(thread);
-        }
-        threads.forEach(t -> {
-            try {
-                t.join();// вместо ожидания делаем данный меток к каждому патоку
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        System.out.println(mainConcurrency.counter);
+//        final MainConcurrency mainConcurrency = new MainConcurrency();
+//        List <Thread> threads = new ArrayList <>(THREADS_NUMBER);
+//        for (int i = 0; i < THREADS_NUMBER; i++) {
+//            Thread thread = new Thread(() -> {
+//                for (int j = 0; j < 100; j++) {
+//                    mainConcurrency.inc();
+//                }
+//            });
+//            thread.start();
+//            threads.add(thread);
+//        }
+//        threads.forEach(t -> {
+//            try {
+//                t.join();// вместо ожидания делаем данный меток к каждому патоку
+//            } catch(InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        System.out.println(mainConcurrency.counter);
+//    }
 
+//      deadlock
+        final MainConcurrency mainConcurrency = new MainConcurrency();
         Thread thread1 = new Thread(() -> {
             synchronized(LOCK) {
                 System.out.println("Thread 1: Holding LOCK...");
