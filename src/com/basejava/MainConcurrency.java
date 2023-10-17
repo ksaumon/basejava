c94f59a68c280261b1bbc374d66749eca51f7dce
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-17 18:34:52 +0300

    11 урок

--- src/com/basejava/MainConcurrency.java
+++ src/com/basejava/MainConcurrency.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class MainConcurrency {
     private int counter;
     private static final Object LOCK = new Object();
+    private static final Object LOCK1 = new Object();
     private static final int THREADS_NUMBER = 10000;
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
+
         Thread thread0 = new Thread() {
             @Override
             public void run() {
@@ -116,6 +118,42 @@ public class MainConcurrency {
                 e.printStackTrace();
             }
         });
+        System.out.println(mainConcurrency.counter);
+
+        Thread thread1 = new Thread(() -> {
+            synchronized(LOCK) {
+                System.out.println("Thread 1: Holding LOCK...");
+                try {
+                    Thread.sleep(100);
+                } catch(InterruptedException e) {
+                }
+                System.out.println("Thread 1: Waiting for LOCK1...");
+                synchronized(LOCK1) {
+                    System.out.println("Thread 1: Holding LOCK and LOCK1...");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized(LOCK1) {
+                System.out.println("Thread 2: Holding LOCK1...");
+                try {
+                    Thread.sleep(100);
+                } catch(InterruptedException e) {
+                }
+                System.out.println("Thread 2: Waiting for LOCK...");
+                synchronized(LOCK) {
+                    System.out.println("Thread 2: Holding LOCK1 and LOCK...");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
         System.out.println(mainConcurrency.counter);
     }
 
