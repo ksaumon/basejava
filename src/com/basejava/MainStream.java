f98f1b4c0ec9faa94aa4610b6ed5666802d3f18a
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-19 16:55:57 +0300

    12 урок

---
+++ src/com/basejava/MainStream.java
@@ -0,0 +1,31 @@
+package com.basejava;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+    public static void main(String[] args) {
+        int[] values = {4, 2, 2, 8, 4, 5, 9, 1, 9};
+        int result = minValue(values);
+        int[] result1 = minValue1(values);
+        System.out.println("Минимально возможное число: " + result);
+        List <Integer> integers = Arrays.stream(result1).boxed().collect(Collectors.toList());
+        List <Integer> filteredList = oddOrEven(integers, result);
+        System.out.println("Результат: " + filteredList);
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce(0, (a, b) -> a * 10 + b);
+    }
+
+    public static int[] minValue1(int[] values) {
+        return Arrays.stream(values).distinct().sorted().toArray();
+    }
+
+    public static List <Integer> oddOrEven(List <Integer> integers, int result) {
+        int sumOfDigits = String.valueOf(result).chars().map(Character::getNumericValue).sum();
+        return integers.stream().filter(number -> (sumOfDigits % 2 == 0) != (number % 2 == 0))
+                .collect(Collectors.toList());
+    }
+}
