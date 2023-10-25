7466c7deabfc2fd7bc5d5dd259ef82b5e4e39b15
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2023-10-25 14:56:23 +0300

    14 урок

---
+++ src/com/basejava/sql/SqlTransaction.java
@@ -0,0 +1,10 @@
+package com.basejava.sql;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface SqlTransaction<T> {
+    T execute(Connection conn) throws SQLException;
+}
+
